eeab2eb8d52421383edf647ce70efd49b449779b
Josh Marchand <josh.r.marchand@gmail.com>
2020-02-05 09:47:40 -0600

    2/5 warmup complete

---
+++ js/external-lec.js
@@ -0,0 +1,2 @@
+'use strict';
+var myAge = 25;
---
+++ js/external.js
@@ -0,0 +1,32 @@
+// console.log('Hello from external JavaScript');
+//
+// alert('Welcome to my Website!');
+// var favColor = prompt('What is your favorite color?');
+// favColor === 'blue' ? alert('Great! Mine too') : false ;
+
+
+// var lm, bb, herc, pay, price = 3;
+// lm = Number(prompt('How many days did you watch The Little Mermaid?'));
+// bb = Number(prompt('How many days did you watch Brother Bear?'));
+// herc = Number(prompt('How many days did you watch Hercules?'));
+// pay = (lm + bb + herc) * price;
+// alert('You owe ' + pay + ' dollars');
+
+// var googleRate = 400, amazonRate = 380, facebookRate = 350,
+//     googleHrs, amazonHrs, facebookHrs, pay = 0;
+// googleHrs = Number(prompt('How many hours did you work for Google?'));
+// amazonHrs = Number(prompt('How many hours did you work for Amazon?'));
+// facebookHrs = Number(prompt('How many hours did you work for Facebook?'));
+// pay = (googleRate * googleHrs) + (amazonRate * amazonHrs) + (facebookRate * amazonHrs);
+// alert('Yo\'ve earned ' + pay + ' dollars');
+
+// var full, conflict;
+// full = confirm('Is the class full?');
+// conflict = confirm('Is there a conflict?');
+// full && conflict ? alert('You may attend') : alert('You may not attend');
+
+// var quant, expired, premium;
+// premium = confirm('Are you a premium member?');
+// expired = confirm('Is it expired?');
+// quant = Number(prompt('What is the quantity?'));
+// expired && quant > 2 || premium ? alert('Can be applied.') : alert('Cannot be applied.');
