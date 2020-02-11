7e52e4d35bdccdd5f150a37c477739cf7dd56e79
Josh Marchand <josh.r.marchand@gmail.com>
2020-02-11 13:14:37 -0600

    loops complete

---
+++ js/for_loops.js
@@ -0,0 +1,50 @@
+
+
+function showMultiplicationTable(num) {
+
+    for (var i = 1; i <= 10; i++)
+    console.log(num + ' x ' + i + ' = ' + (num * i));
+}
+
+showMultiplicationTable(4);
+
+for (var num = Math.floor(Math.random() * 20) + 180, i = 1; i <= 10; i++, num = Math.floor(Math.random() * 20) + 180){
+    odd = num % 2 === 0 ? 'even' : 'odd';
+    console.log(num + ' is ' + odd);
+}
+
+for (i = 1; i < 10; i++){
+    switch (i) {
+        case 1:
+            console.log(1);
+            break;
+         case 2:
+            console.log(22);
+            break;
+         case 3:
+            console.log(333);
+            break;
+         case 4:
+            console.log(4444);
+            break;
+         case 5:
+            console.log(55555);
+            break;
+         case 6:
+            console.log(666666);
+            break;
+         case 7:
+            console.log(7777777);
+            break;
+         case 8:
+            console.log(88888888);
+            break;
+         case 9:
+            console.log(999999999);
+            break;
+    }
+}
+
+for (i=100; i> 0; i-=5){
+    console.log(i);
+}
