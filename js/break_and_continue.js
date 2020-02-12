8f65bae68c8fbbbc2a76ad9a7ee9567acd0f6ac5
Josh Marchand <josh.r.marchand@gmail.com>
2020-02-12 16:07:24 -0600

    arrays complete

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,12 +1,29 @@
-do {
-    var p = prompt('odd number betwen 1 and 50');
-} while (p % 2 !== 1 || (p <= 1 && p >= 50));
+// do {
+//     var p = prompt('odd number betwen 1 and 50');
+// } while (p % 2 !== 1 || (p <= 1 && p >= 50));
+//
+//
+// do {
+//     var userNumber = Number(prompt('Please enter number between 1 and 50'));
+//
+//     if (userNumber < 1 || userNumber > 50){
+//         alert(userNumber + 'is not between 1 and 50, please enter again');
+//     } else if (userNumber % 2 === 0){
+//         alert(userNumber + ' is not odd! Please enter again!');
+//     } else {
+//         alert('congrats, you picked the right number!');
+//         break;
+//     }
+// } while (true);
 
-for (i = 1; i <= 50; i++){
-
-    if (i == p){
-        console.log('Yikes! Skipping number ' + p);
-    } else if (i % 2 === 1){
-        console.log(i);
-    };
-}
+//
+//
+//
+// for (i = 1; i <= 50; i++){
+//
+//     if (i == p){
+//         console.log('Yikes! Skipping number ' + p);
+//     } else if (i % 2 === 1){
+//         console.log(i);
+//     };
+// }
