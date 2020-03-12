cfca1dbbba0d598f236d38277731ebb9bf6c46ab
Josh Marchand <josh.r.marchand@gmail.com>
2020-03-12 09:04:50 -0500

    promises finished

--- js/promises.js
+++ js/promises.js
@@ -11,15 +11,32 @@
 //     });
 // };
 
+//comment
+
 // let wait = num =>{
 //     return new Promise(res => {
 //         setTimeout(res, num);
 //     });
 // };
 //
-// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
 // wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
-fetch('https://api.github.com/users', {headers: {'Authorization': 'token  6f54f81ddd21aff7fb4ac4ef148a1f7a0f0035cc'}})
-    .then(response => console.log(response.json()))
+let commit = () =>
+    fetch(`https://api.github.com/users/${prompt('Username?')}/events/public`, {headers: {'Authorization': 'token  6f54f81ddd21aff7fb4ac4ef148a1f7a0f0035cc'}})
+        .then(response => response.json())
+        .then(data => {
+            for (var i = 0; i <= data.length - 1; i++) {
+                if (data[i].type === 'PushEvent') {
+                    break;
+                }
+            }
+            fetch(data[i].payload.commits[0].url, {headers: {'Authorization': 'token  6f54f81ddd21aff7fb4ac4ef148a1f7a0f0035cc'}})
+                .then(thingy => thingy.json())
+                .then(thingy => alert(new Date(thingy.commit.author.date)));
+        })
+        .catch(error => {
+            console.log(error);
+            commit();
+        });
 
+commit();
